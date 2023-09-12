c9d0b0c75170f83812cac5e1c69993f8ec39f057
Jonah Bregstone <jbregsto@oberlin.edu>
2023-09-12 15:27:50 -0400

    Initial commit

---
+++ src/components/exampleChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h5>Chart Data</h5>
+      <pre>
+        <code>
+          {{ harness.getChartData(chart.key) }}
+        </code>
+      </pre>
+    </div>
+    <div class="col">
+      <h5>Filters:</h5>
+      <ul>
+        <li v-for="(filter, idx) in harness.filters" :key="idx">
+          <strong>{{ filter.label }}</strong
+          >:
+          {{
+            harness.getLabelForSelectedOption(filter.key) ||
+            harness.getFilter(filter.key)
+          }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { useHarnessComposable } from "@rtidatascience/harness-vue";
+defineProps({
+  chart: {
+    type: Object,
+    required: true,
+  },
+});
+const harness = useHarnessComposable();
+</script>
